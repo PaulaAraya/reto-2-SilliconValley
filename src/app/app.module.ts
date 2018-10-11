672ed7eeec74b4efcf6f8cfe6c1b64e0f9a56087
PaulaAraya <paula.arayawilliams@gmail.com>
2018-10-11 00:43:11 -0300

    pictures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PicturesComponent } from './components/pictures/pictures.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PicturesComponent
   ],
   imports: [
     BrowserModule
