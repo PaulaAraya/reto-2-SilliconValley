16f65826aab4b323ccf8df75caaad339ca8e1d19
paulaaraya <paula.arayawilliams@gmail.com>
2018-10-24 00:22:19 -0300

    arreglo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { PicturesComponent } from './components/pictures/pictures.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ApiUnsplashService } from './services/api-unsplash.service';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    PicturesComponent
+    PicturesComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
